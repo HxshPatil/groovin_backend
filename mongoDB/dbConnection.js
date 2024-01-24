0f4281fd1bed6d70a8ee354f781b107cc248d1d0
HxshPatil <harshvardhan@attryb.com>
2024-01-24 17:11:45 +0530

    changes

--- mongoDB/dbConnection.js
+++ mongoDB/dbConnection.js
@@ -1,12 +1,14 @@
-const mongoose= require("mongoose");
+const mongoose = require("mongoose");
 
-const connectDb=async()=>{
-    try{
-        const connect=await ("mongoose.connectmongodb+srv://harsh0patil001:L1LXkCZoX9SYSVAm@harsh.8sbkl6k.mongodb.net/BUYC");
-        console.log("connection to database successful and database name:",connect.connection.name);
-    }catch(err){
-        console.log("cannot connect to database");
+const connectDb = async () => {
+    try {
+        const connect = await mongoose.connect("mongodb+srv://harsh0patil001:L1LXkCZoX9SYSVAm@harsh.8sbkl6k.mongodb.net/BUYC");
+
+        console.log("Connection to database successful. Database name:", connect.connection.name);
+    } catch (err) {
+        console.error("Cannot connect to the database:", err);
         process.exit(1);
     }
-}
-module.exports= connectDb;
+};
+
+module.exports = connectDb;
