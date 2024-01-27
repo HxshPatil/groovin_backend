0d3915e17de138a10e30916eeda761d0e69cec99
HxshPatil <harshvardhan@attryb.com>
2024-01-28 01:55:36 +0530

    working and stable

--- controllers/get_all_cars.js
+++ controllers/get_all_cars.js
@@ -3,7 +3,7 @@ const oem = require("../mongoDB/models/oem_specs");
 const getAllCars = async (req, res) => {
   try {
     // Fetch all cars from the database
-    const oem_details = await oem.find();
+    const oem_details = await oem.find().sort({'manufacturer_model': 1});
     res.status(200).json({ cars: oem_details });
   } catch (error) {
     console.error("Error fetching all cars:", error.message);
